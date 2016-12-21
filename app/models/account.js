c33774ba477f806539cbc2a13f7f8d30d4b2da1f
Micah Cooper <micah@mrmicahcooper.com>
2016-12-20 19:03:02 -0600

    Order items by amount descending

--- app/models/account.js
+++ app/models/account.js
@@ -2,11 +2,15 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 const { attr, hasMany } = DS;
-const { computed, computed: { filterBy, sum, mapBy} } = Ember;
+const { computed, computed: { filterBy, sum, mapBy, sort } } = Ember;
 
 export default DS.Model.extend({
   name: attr(),
   items: hasMany('item'),
+
+  orderedItems: sort('items', function(a, b) {
+    return a.get('amount') < b.get('amount');
+  }),
 
   expenseItems: filterBy('items', 'expense'),
   expenseItemAmounts: mapBy('expenseItems', 'amount'),
